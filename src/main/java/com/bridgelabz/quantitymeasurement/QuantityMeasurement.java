bb6bddae2ea6853bcf993304540c4750583991f6
akshayakky <akshaysmpt01@gmail.com>
2020-04-09 15:17:43 +0530

    ADDED : Test Case And Code For Comparing Two Feet Values

--- src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.quantitymeasurement;
 
 public class QuantityMeasurement {
-
+    public boolean compare(Feet feet1, Feet feet2) {
+        if(feet1.equals(feet2))
+            return true;
+        return false;
+    }
 }
--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.quantitymeasurement;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class QuantityMeasurementTest {
-// Welcome To Quantity Measurement Problem
+    @Test
+    public void givenLengthsInFeet_ShouldReturnTrue() {
+        Feet feet1 = new Feet(12);
+        Feet feet2 = new Feet(12);
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        boolean areEqual = quantityMeasurement.compare(feet1, feet2);
+        Assert.assertEquals(true, areEqual);
+    }
 }
