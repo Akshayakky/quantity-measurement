e2af8d8a429c25719d4f790de589a9dad8d1c735
akshayakky <akshaysmpt01@gmail.com>
2020-04-09 16:59:51 +0530

    ADDED : Comparing Length Test Case 5

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -155,4 +155,13 @@ public class QuantityMeasurementTest {
         boolean areEqual = quantityMeasurement.compare(length1, length2);
         Assert.assertEquals(true, areEqual);
     }
+
+    @Test
+    public void givenTwoLengthsTest5_ShouldReturnFalse() {
+        Length length2 = new Length(1, QuantityMeasurement.LengthUnits.FEET);
+        Length length1 = new Length(1, QuantityMeasurement.LengthUnits.FEET);
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        boolean areEqual = quantityMeasurement.compare(length1, length2);
+        Assert.assertEquals(true, areEqual);
+    }
 }
