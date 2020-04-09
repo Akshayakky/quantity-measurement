25023f2102b6963e2d2c081a81a2b1cff217c818
akshayakky <akshaysmpt01@gmail.com>
2020-04-09 15:24:50 +0530

    ADDED : Test Case For Equality Check When Given Object is Null

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -6,10 +6,19 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
     @Test
     public void givenLengthsInFeet_ShouldReturnTrue() {
-        Feet feet1 = new Feet(12);
-        Feet feet2 = new Feet(12);
+        Feet feet1 = new Feet(0);
+        Feet feet2 = new Feet(0);
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
         boolean areEqual = quantityMeasurement.compare(feet1, feet2);
         Assert.assertEquals(true, areEqual);
+    }
+
+    @Test
+    public void givenObjectNull_ShouldReturnFalse() {
+        Feet feet1 = new Feet(0);
+        Feet feet2 = null;
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        boolean areEqual = quantityMeasurement.compare(feet1, feet2);
+        Assert.assertEquals(false, areEqual);
     }
 }
